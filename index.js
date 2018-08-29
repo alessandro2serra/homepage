69f8518e62532f3374f40dc60934327236dd2c9c
kristjanjansen <kristjan.jansen@gmail.com>
2018-08-29 22:03:23 +0300

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import Art from "./components/Art.js";
 import Buttons from "https://rawgit.com/designstem/style/master/components/Buttons.js";
 
+import cards from "./cards.js"
+
 const unique = array => [...new Set(array)]
 const flatten = array => [].concat(...array)
 
@@ -15,99 +17,7 @@ new Vue({
   data: () => ({
     activeType: 0,
     types: ["All", "Math", "Biology", "Physics", "Geometry"],
-    cards: [
-      {
-        title: "Looking for triangles",
-        desc: "Build an bird nest box from classic Euclidean shapes",
-        tags: ["Geometry", "Crafts"],
-        url: "./triangles"
-      },
-      {
-        title: "Smart Tattoo",
-        desc: "Design a biosensor tattoo that helps to save lives",
-        tags: ["Biology", "Information design"],
-        url: "./tattoo"
-      },
-      {
-        title: "Vinyl frequency",
-        desc:
-          "Spin that record and learn about frequencies and early days of cinema",
-        tags: ["Math", "Physics", "Animation"],
-        url: "https://designstem.github.io/vinylfrequency"
-      },
-      {
-        title: "3D Tessellations",
-        desc: "Arrange mosaics in space",
-        tags: ["Geometry", "Physics", "Animation"],
-        disabled: true
-      },
-      {
-        title: "Waste is a design mistake",
-        desc: "Explore sustainable material usage",
-        tags: ["Materials", "Crafts", "Sustainablity"],
-        disabled: true
-      },
-      {
-        title: "Different kind of colors",
-        desc: "Emphatize and design for color blindness",
-        tags: ["Color", "Inclusive design"],
-        disabled: true
-      },
-      {
-        title: "Robots on the Beach",
-        desc: "Build your own <i>strandbeest</i>",
-        tags: ["Crafts", "Physics", "Kinetics"],
-        disabled: true
-      },
-      {
-        title: "Painting with numbers",
-        desc: "Generate artwork from sensor signals",
-        tags: ["Generative art", "Sensors"],
-        disabled: true
-      },
-      {
-        title: "Buzzing Hexagons",
-        desc: "Learn about the divine geometry of beehives",
-        tags: ["Geometry", "Tesselation"],
-        disabled: true
-      },
-      {
-        title: "Call me Red, Green or Blue",
-        desc: "Build the RGB lamp for inner peace and enlightenment",
-        tags: ["Color", "Electronics", "Psychology"],
-        disabled: true
-      },
-      {
-        title: "Down the Spiral",
-        desc: "Learn about the divine geometry of spirals",
-        tags: ["Geometry", "3D Printing"],
-        disabled: true
-      },
-      {
-        title: "Blame it on the oven",
-        desc: "Pottery process with occasional explosions",
-        tags: ["Phyics", "Pottery"],
-        disabled: true
-      },
-      {
-        title: "Honey, I shrunk the...",
-        desc: "Scaling from Gulliver to Meirelles to AR",
-        tags: ["Geometry", "Human scale"],
-        disabled: true
-      },
-      {
-        title: "Construction of Chaos",
-        desc: "Learn about the divine architecture of nature",
-        tags: ["Material", "Construction"],
-        disabled: true
-      },
-      {
-        title: "Unbearable lightness of structures",
-        desc: "Lightness in all its forms",
-        tags: ["Material", "Construction"],
-        disabled: true
-      }
-    ],
+    cards: cards,
     colors: [
       "var(--color-red)",
       "var(--color-blue-dark)",
