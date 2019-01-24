89052abe10ff19d87d149007cc0e2f465af34a40
Oliver Maaker <maaker@gmail.com>
2019-01-24 13:18:55 +0200

    colorblindness2 image-compare component

---
+++ colorblindness2/index.js
@@ -0,0 +1,46 @@
+// Use Javascript module import to get initialization mixin, Vue components and utility functions
+
+import { Init } from "https://designstem.github.io/fachwerk/mixins.js";
+import * as components from "https://designstem.github.io/fachwerk/components.js";
+import * as utils from "https://designstem.github.io/fachwerk/utils.js";
+
+for (const name in components) {
+  Vue.component(name, components[name]);
+}
+
+import ImageCompare from "./components/ImageCompare.js";
+Vue.component('ImageCompare', ImageCompare);
+
+new Vue({
+  // Attaching Vue to <div id="app"></div>
+  el: "#app",
+  data: function() {
+    return {
+      before: 'img/before.jpg',
+      after: 'img/after.jpg',
+      padding: {
+        left: 50,
+        right: 50
+      },
+      hideAfter: true
+    }
+  },
+  // Adding a mixin
+  mixins: [Init],
+
+  // Making utilities accessible to templates
+  methods: { ...utils },
+
+  // Fetching the index.md and rendering it
+  template: `                         
+  
+  <f-fetch-data url="./index.md">
+    <f-content-document
+      slot-scope="data"
+      :content="data.value"
+    />
+  </f-fetch-data>
+
+
+`
+});
---
+++ colorblindness2/index.md
@@ -0,0 +1,9 @@
+# Image compare
+
+<ImageCompare :before="'images/before.jpg'" :after="'images/after.jpg'" :full="false" :padding ="{left:20, right:20}" :hideAfter="false"></ImageCompare>
+
+# Hello world
+
+<f-scene grid>
+  <f-circle />
+</f-scene>
