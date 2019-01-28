8ea116d1e9708582de95c4cf44a2d153fd649ccc
Oliver Maaker <maaker@gmail.com>
2019-01-28 14:00:34 +0200

    point hover

--- spirals2/README.md
+++ spirals2/README.md
@@ -3,6 +3,10 @@
 
 Assume that a point is described by its ***polar coordinates (r, θ)***. Remember that **r is the radius**, the distance of the point from the center of the axes and **θ is the angle** formed between the radius and x axis (see next figure).
 
+<br><br>
+
+<f-next-button />
+
 ---
 
 | 1 1 1 1
@@ -27,16 +31,22 @@ Any point on a circle can be described through <f-sidebar width="60vw" src="./Ma
     to="35"
     :step="1" 
 />
+
+<br>
+
+<f-next-button />
+
 -
 
 <f-artboard width="600" height="400" style="width:100%; height:50vh;">
     <g transform="translate(300 200)">
-        <Spiral :cycles="1" :padding="0" :startX="get('radius', 100)" :activePoint="get('active', 8)" :info="true" />
+        <Spiral :cycles="1" :padding="0" :startX="get('radius', 100)" :activePoint="get('active', 8)" :info="false" />
     </g>
 </f-artboard>
 
 
 ---
+
 
 | 1 1 1 1
 | 2 3 3 3
@@ -52,6 +62,7 @@ Any point on a circle can be described through <f-sidebar width="60vw" src="./Ma
     from="0"
     to="200" 
 />
+
 <f-slider 
     title="Cycles"
     :value="2" 
@@ -74,6 +85,10 @@ Any point on a circle can be described through <f-sidebar width="60vw" src="./Ma
     :to="get('cycles')*36"
     :step="1" 
 /> -->
+
+<br>
+
+<f-next-button />
 
 -
 
--- spirals2/Spiral.js
+++ spirals2/Spiral.js
@@ -9,14 +9,17 @@ export default {
     padding: { default: 20, type: Number },
     cycles: { default: 3, type: Number },
     startX: { default: 20, type: Number },
-    activePoint: { default:20 , type: Number },
+    activePointProp: { default:20 , type: Number },
     r: { default:3 , type: Number },
     info: { default:false , type: Boolean },
   },
+  data: () => ({
+    activePoint: 20
+  }),
   computed: {
     d3Line(){
-        return d3.line().curve(d3.curveCardinal.tension(0));
-      },
+      return d3.line().curve(d3.curveCardinal.tension(0));
+    },
     computedPoints(){
         const temp = [[this.startX, 0]];
         let a = parseFloat(this.startX);    
@@ -39,7 +42,10 @@ export default {
         //let points = [ {x:0, y:0}, {x:this.computedPoints[this.activePoint][0], y:0}, {x:this.computedPoints[this.activePoint][0], y:this.computedPoints[this.activePoint][1]} ];
         let points =  `M0,0 L ${this.computedPoints[this.activePoint][0]} 0 L ${this.computedPoints[this.activePoint][0]} ${this.computedPoints[this.activePoint][1]} Z`
         return points;
-      }
+      },
+      transformText () {
+        return 'translate( this.computedPoints[this.activePoint][0] , this.computedPoints[this.activePoint][1]-20 )';
+     }
   },
   methods: {
     ...utils
@@ -60,8 +66,9 @@ export default {
         :cy="p[1]"
         :r="r"
         stroke="rgba(0,0,0,0)"
-        stroke-width="6"
+        stroke-width="7"
         v-on:click="activePoint = i"
+        class="hoverable"
       />
       <circle 
         :cx="computedPoints[activePoint][0]"
@@ -77,15 +84,26 @@ export default {
         stroke-width="1.5"
         fill="none"
       /> 
-      <text v-if="info"
-        :dy="computedPoints[activePoint][1]-20" :dx="computedPoints[activePoint][0]" 
-        text-anchor="middle" 
-        style="font-family: var(--font-mono); fill:var(--red); font-size:14px">
-        x:{{ round(computedPoints[activePoint][0], 3) }} | 
-        y:{{ round(computedPoints[activePoint][1], 3) }} |
-        θ:{{round( Math.atan(computedPoints[activePoint][1]/computedPoints[activePoint][0])*(180 / Math.PI), 3) }}° |
-        r:{{round( Math.sqrt(Math.pow(computedPoints[activePoint][0], 2) + Math.pow(computedPoints[activePoint][1], 2)), 3) }}
-      </text>
+      <g :transform="'translate( -200 , -200 )'" v-if="info" >
+        <rect fill="#fff" height="20" width="400" :stroke="hsl(0,0,0,0.5)" stroke-width="1"  />
+        <text 
+          dy="14" dx="10" 
+          style="font-family: var(--font-mono); fill:var(--red); font-size:14px">
+          x:{{ round(computedPoints[activePoint][0], 3) }} | 
+          y:{{ round(computedPoints[activePoint][1], 3) }} |
+          θ:{{round( Math.atan(computedPoints[activePoint][1]/computedPoints[activePoint][0])*(180 / Math.PI), 3) }}° |
+          r:{{round( Math.sqrt(Math.pow(computedPoints[activePoint][0], 2) + Math.pow(computedPoints[activePoint][1], 2)), 3) }}
+        </text>
+        <!-- <text v-if="info"
+          :dy="computedPoints[activePoint][1]-20" :dx="computedPoints[activePoint][0]" 
+          text-anchor="middle" 
+          style="font-family: var(--font-mono); fill:var(--red); font-size:14px">
+          x:{{ round(computedPoints[activePoint][0], 3) }} | 
+          y:{{ round(computedPoints[activePoint][1], 3) }} |
+          θ:{{round( Math.atan(computedPoints[activePoint][1]/computedPoints[activePoint][0])*(180 / Math.PI), 3) }}° |
+          r:{{round( Math.sqrt(Math.pow(computedPoints[activePoint][0], 2) + Math.pow(computedPoints[activePoint][1], 2)), 3) }}
+        </text> -->
+      </g>
     </g>
     `
 };
